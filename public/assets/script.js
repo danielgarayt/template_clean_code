9e21bc0f0e27c217111a4168127c4544d371dbe3
danielgarayt <89745121+danielgarayt@users.noreply.github.com>
2025-11-27 10:29:17 -0300

    teste2

---
+++ public/assets/script.js
@@ -0,0 +1,4 @@
+// Criação do jogo do dinossauro com um iframe
+document.getElementById('game-container').innerHTML = `
+    <iframe src="https://chrome-dino-game.glitch.me/" width="100%" height="300px" frameborder="0"></iframe>
+`;
